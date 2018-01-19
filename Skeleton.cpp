46e24789cc906ebf41d02267dbd556f5d33dff22
Louie-ru <17528326+Louie-ru@users.noreply.github.com>
2018-01-20 02:24:16 +0300

    Updated version

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -2,31 +2,35 @@
 All strings(authors, keywords, editors...) are stored in one string (not vector<string>) separated by comma for making requests simpler like
 SELECT * FROM Books WHERE authors CONTAINS "Thomas";
 
-
+In search requests:
+"" for string not set
+-1 for int not set
+bool or_and:  TRUE if client wants all criterias be true; FALSE if client wants any criteria be true
 */
 
 class Document{
-	string authors, title, keywords;
-	int copies;//number of copies in library
-	int id, price, room, level;
+	string title, keywords;
+	int copies, id, price, room, level;
 }
 
+//defined type constants
 #define BOOK 1
 #define ARTICLE 2
 #define AV 3
 
 class Book : Document{
-	string publisher;
+	string publisher, authors;
 	int year;
 	bool bestseller;
 }
 
 class Article : Document{
-	string journal_title, publisher, editors;
+	string journal_title, publisher, editors, authors;
 	int year, month;
 }
 
 class AV : Document{
+	string authors;
 }
 
 class Check_out {
@@ -41,25 +45,25 @@ class Check_out {
 class User{
 	string name, address, phone;
 	int id;//id in Patrons/Librarians table
-}
 
-class Patron : User{
-	bool faculty;
-	vector<int> check_outs;//ids of user's check_outs
-
-	/*
-	"" for string not set
-	-1 for int not set
-	bool or_and:  TRUE if client wants all criterias be true; FALSE if client wants any criteria be true
-	*/
 	vector<Book> search_books(string author, string title, string keywords, string publisher, string editors, int year, int bestseller, int available, bool or_and);
 	vector<Article> search_articles(string author, string title, string keywords, string journal_title, string publisher, string editors, int year, int month, int available, bool or_and);
 	vector<AV> search_av(string author, string title, string keywords, int available, bool or_and);
+}
 
-	//return TRUE if success, FALSE if error
-	bool check_out_book(int id);
-	bool check_out_article(int id);
-	bool check_out_av(int id);
+//can search/check_out documents, renew/return check_outed documents
+class Patron : User{
+	bool faculty;//is faculty member
+
+	//return number of weeks for check_out or -1 for error
+	int check_out_book(int id);
+	int check_out_article(int id);
+	int check_out_av(int id);
+
+	//return vector of my documents
+	vector<Book> get_checked_out_books();
+	vector<Article> get_checked_out_articles();
+	vector<AV> get_checked_out_avs();
 
 	//return fine size or 0
 	int return_book(int id);
@@ -67,24 +71,23 @@ class Patron : User{
 	int return_av(int id);
 }
 
+//can search/add/delete/modify users, search/add/delete/modify documents, search overdue documents
 class Librarian : User{
 	//search in Check_outs table
-	vector<Book> search_overdue_books(string author, string title, string keywords, string publisher, string editors, int year, int bestseller, int available, bool or_and);
-	vector<Article> search_overdue_articles(string author, string title, string keywords, string journal_title, string publisher, string editors, int year, int month, int available, bool or_and);
-	vector<AV> search_overdue_av(string author, string title, string keywords, int available, bool or_and);
+	vector<Book> search_books_checked_out(int user_id, string author, string title, string keywords, string publisher, string editors, int year, int bestseller, int available, bool or_and);
+	vector<Article> search_articles_checked_out(int user_id, string author, string title, string keywords, string journal_title, string publisher, string editors, int year, int month, int available, bool or_and);
+	vector<AV> search_av_checked_out(int user_id, string author, string title, string keywords, int available, bool or_and);
 
 	//return TRUE if success, FALSE if error
+	vector<Patron> search_patrons(string name, string address, string phone, bool faculty, bool has_overdues, bool or_and);
 	bool add_patron(string name, string address, string phone, bool faculty);
-	bool add_librarian(string name, string address, string phone, bool faculty);
-
-	vector<Patron> search_patrons(string name, string address, string phone, bool faculty, bool or_and);
 	bool delete_patron(int user_id);
 	bool modify_patron(int user_id, string name, string address, string phone, bool faculty);
 
-	Check_out get_check_out(int id);
-	Book get_book(int id);
-	Article get_article(int id);
-	AV get_av(int id);
+	vector<Librarian> search_librarians(string name, string address, string phone, bool or_and);
+	bool add_librarian(string name, string address, string phone);
+	bool delete_librarian(int user_id);
+	bool modify_librarian(int user_id, string name, string address, string phone);
 
 	bool add_book_new(string author, string title, string keywords, string publisher, string editors, int year, bool bestseller, int copies);
 	bool add_book_copy(int id, int copies);
@@ -102,6 +105,7 @@ class Librarian : User{
 	bool modify_av(int id, string author, string title, string keywords, bool available, int copies);
 }
 
+//return 0-error 1-patron 2-librarian
 int login(string username, string password);
 
 DATABASE TABLES
@@ -122,10 +126,5 @@ Table: Librarians
 Columns: string name, string address, string phone, int id;
 
 Table: Check_outs
-Columns: int document_type, int document_id, check_out_id, int year_start, int month_start, int day_start, int year_end, int month_end, int day_end;
+Columns: int user_id, int document_type, int document_id, check_out_id, int year_start, int month_start, int day_start, int year_end, int month_end, int day_end;
 ----------------------------------------------------------------------------------------------------------------
-
-
-/*
-
-*/
