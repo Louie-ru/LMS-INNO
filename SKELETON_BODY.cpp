c27c93b47afdb8664f408b285b31dc0b4117172f
AkkyMa <l9001@mail.ru>
2018-01-29 01:13:05 +0300

    Update SKELETON_BODY.cpp

--- SKELETON_BODY.cpp
+++ SKELETON_BODY.cpp
@@ -380,66 +380,6 @@ public:
 	LibrarianUser(QString &name, QString &address, QString &phone, int id, QString &login, string &password) : User(name, address, phone, id, login, password) {}
 };
 
-namespace db {
-
-	// false if there are exist equal document
-	// TO-DO:
-	// generate unique id for new book
-	// add it to database
-	bool add_book(Book*);
-	bool add_article(Article*);
-	bool add_av(VA*);
-	bool add_checkout(Document*, int type, tm&begin, tm&end);
-
-	// false if there nothing to modify
-	bool modify_book(Book*);
-	bool modify_article(Article*);
-	bool modify_av(VA*);
-
-	// false if there nothing to delete
-	bool delete_book(int id);
-	bool delete_article(int id);
-	bool delete_av(int id);
-	bool delete_checkout(int id);
-
-	// return null if there are no appropriate document
-	Book* get_book(int id);
-	Article* get_article(int id);
-	VA* get_AV(int id);
-	Check_out* get_check_out(int id);
-
-	//return empty vector if there are no appropriate documents
-	QVector<Book>* search_books(QString *authors, QString *title, QString *keywords, QString *publisher, int *year, bool bestseller, bool available, bool or_and);
-	QVector<Article>* search_articles(QString *authors, QString *title, QString *keywords, QString *journal_title, QString *publisher, QString *editors, int *year, int *month, bool available, bool or_and);
-	QVector<VA>* search_av(QString *authors, QString *title, QString *keywords, bool available, bool or_and);
-
-	//return empty vector if there are no appropriate documents
-	QVector<pair<Check_out, Book>>* search_books_checked_out(int *user_id, QString *authors, QString *title, QString *keywords, QString *publisher, int *year, bool bestseller, bool or_and);
-	QVector<pair<Check_out, Article>>* search_articles_checked_out(int *user_id, QString *authors, QString *title, QString *keywords, QString *journal_title, QString *publisher, QString *editors, int *year, int *month, bool or_and);
-	QVector<pair<Check_out, VA>>* search_av_checked_out(int *user_id, QString *authors, QString *title, QString *keywords, bool or_and);
-
-	// false if there are exist user with such id or login
-	bool add_patron(PatronUser*);
-	bool add_librarian(LibrarianUser*);
-
-	// false if there are no user with such id or types is not coincides
-	bool modify_patron(PatronUser*);
-	bool modify_librarian(LibrarianUser*);
-
-	// false if there are no user with such id or types is not coincides
-	bool delete_patron(int id);
-	bool delete_librarian(int id);
-
-	// return null if there are no user with such id or types is not concides
-	PatronUser* get_patron(int id);
-	LibrarianUser* get_librarian(int id);
-
-	QVector<PatronUser>* search_patrons(QString *name, QString *address, QString *phone, bool *faculty, bool has_overdues, bool or_and);
-
-	// return null if password don't coincides
-	pair<User, int>* get_user(QString login);
-}
-
 // return null in case of error
 pair<User, int>* login(QString login, string password) {
 	pair<User, int>* user = db::get_user(login);
