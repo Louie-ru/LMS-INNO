73d437893db9f6b9f912357ec646ed7d61e42b87
AkkyMa <l9001@mail.ru>
2018-03-06 19:59:45 +0300

    Tester system implemented

--- tester.cpp
+++ tester.cpp
@@ -1,6 +1,65 @@
 #include "tester.h"
+#include <QtSql>
+#include <skeleton.cpp>
 
-Tester::Tester()
-{
+void Tester::run_tests() {
+    const QString db_name = "db_test.sqlite";
 
+    QSqlDatabase sdb;
+    sdb = QSqlDatabase::addDatabase("QSQLITE");
+    sdb.setDatabaseName(db_name);
+    sdb.open();
+    Login::make_database();
+    LibrarianUser libr;
+    libr.add_librarian("Сидорович", "Бункер на Кордоне", "88005553535", "sidr", "123");
+
+    qDebug() << "TEST1: " << (test1() ? "OK" : "FAIL");
+    qDebug() << "TEST2: " << (test2() ? "OK" : "FAIL");
+    qDebug() << "TEST3: " << (test3() ? "OK" : "FAIL");
+    qDebug() << "TEST4: " << (test4() ? "OK" : "FAIL");
+    qDebug() << "TEST5: " << (test5() ? "OK" : "FAIL");
+    qDebug() << "TEST6: " << (test6() ? "OK" : "FAIL");
+    qDebug() << "TEST7: " << (test7() ? "OK" : "FAIL");
+    qDebug() << "TEST8: " << (test8() ? "OK" : "FAIL");
+    qDebug() << "TEST9: " << (test9() ? "OK" : "FAIL");
+
+    sdb.close();
+    //QFile(db_name).remove();
+}
+
+
+bool Tester::test1() {
+    return true;
+}
+
+bool Tester::test2() {
+    return true;
+}
+
+bool Tester::test3() {
+    return true;
+}
+
+bool Tester::test4() {
+    return true;
+}
+
+bool Tester::test5() {
+    return true;
+}
+
+bool Tester::test6() {
+    return true;
+}
+
+bool Tester::test7() {
+    return true;
+}
+
+bool Tester::test8() {
+    return true;
+}
+
+bool Tester::test9() {
+    return true;
 }
--- tester.h
+++ tester.h
@@ -4,8 +4,19 @@
 
 class Tester
 {
+private:
+    static bool test1();
+    static bool test2();
+    static bool test3();
+    static bool test4();
+    static bool test5();
+    static bool test6();
+    static bool test7();
+    static bool test8();
+    static bool test9();
+
 public:
-    Tester();
+    static void run_tests();
 };
 
 #endif // TESTER_H
