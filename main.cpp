964a1fde47e5340d5140ced1618fbf4d4083fd78
AkkyMa <l9001@mail.ru>
2018-03-06 19:56:34 +0300

    Added tester system

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QtSql>
+#include <tester.h>
 
 QSqlDatabase sdb;
 
 int main(int argc, char *argv[]){
+    Tester::run_tests();
     QApplication a(argc, argv);
     sdb = QSqlDatabase::addDatabase("QSQLITE");
     sdb.setDatabaseName("db.sqlite");
